4c84a90acf1f89f2e81a3126a4d9c95751eecb74
KauaGarcia <141241845+KauaGarcia@users.noreply.github.com>
2024-11-20 20:58:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Swift?',
+    'O Swift é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Escócia?',
+    'A capital da Escócia é Edimburgo'
+)
+
+criaCartao(
+    'Programação',
+    'O que são dicionários?',
+    'Eles são estruturas de dados que armazenam pares de chave-valor, permitindo acesso rápido aos valores usando a chave.'
+)
+
+criaCartao(
+    'Linguagem Gaélico Escocês',
+    'Como se diz Boa Noite em Gaélico Escocês?',
+    'Boa Noite é "Oidhche mhath" (pronunciado "ee-hye vah").'
+)
